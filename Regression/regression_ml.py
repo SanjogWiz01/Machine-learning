c00e71eb74707c08211f5ab4cc564e1914d828aa
Sanjog Poudel <sanjogpoudel384@gmail.com>
2026-02-14 19:16:26 +0545

    Create regression_ml.py

---
+++ Regression/regression_ml.py
@@ -0,0 +1,8 @@
+from sklearn.linear_model import LinearRegression
+import numpy as np
+# Creating sample data
+X = np.array([[1], [2], [3], [4], [5]])
+y = np.array([2, 4, 6, 8, 10])
+# Training the model
+model = LinearRegression()
+model.fit(X, y)
