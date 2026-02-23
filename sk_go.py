8b02d328b84f5d14c0f2f847af712f7eb6ddd288
sanjogwiz01 <sanjogpoudel384@gmail.com>
2026-02-23 22:33:09 +0545

    chamges

---
+++ sk_go.py
@@ -0,0 +1,7 @@
+from sklearn.neighbors import KNeighborsClassifier
+
+knn = KNeighborsClassifier(n_neighbors=3)
+knn.fit(X_train, y_train)
+y_pred_knn = knn.predict(X_test)
+print("Accuracy:", accuracy_score(y_test, y_pred_knn))
+print(classification_report(y_test, y_pred_knn))
