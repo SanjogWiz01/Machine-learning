5ccbd0d64c8367054048e04fdffd0f189146e407
sanjogwiz01 <sanjogpoudel384@gmail.com>
2026-02-26 22:00:06 +0545

    ml

--- SUPERVISED LEARNING/split_model.py
+++ SUPERVISED LEARNING/split_model.py
@@ -1,5 +1,8 @@
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 import numpy as np
 x=np.random.rand(20,1) * 10
 y= 4*x-5
 model = LinearRegression()
+model.fit(x,y)
+model.predict([[4]])
