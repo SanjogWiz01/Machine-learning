fc6c01f3612cfd6c7494a8872629a6374fa4b215
sanjogwiz01 <sanjogpoudel384@gmail.com>
2026-02-27 22:44:41 +0545

    polynomial

--- SUPERVISED LEARNING/polynomial.py
+++ SUPERVISED LEARNING/polynomial.py
@@ -1,13 +1,13 @@
 from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import PolynomialFeatures # importing the polynomial freatures to the code
 
 X = [[1], [2], [3], [4], [5]]
-y = [2, 6, 14, 28, 45]
+y = [2, 6, 14, 28, 45] # values 
 
-poly = PolynomialFeatures(degree=2)
-X_poly = poly.fit_transform(X)
+poly = PolynomialFeatures(degree=2) # making the polynomila of degree 2
+X_poly = poly.fit_transform(X) # i alos donot understadn this 
 
-model = LinearRegression()
-model.fit(X_poly, y)
+model = LinearRegression() # evaluating the model is linear regressions 
+model.fit(X_poly, y) # fitting the model by the value of x an y.
 
-print("Prediction:", model.predict(poly.transform([[6]]))[0])
+print("Prediction:", model.predict(poly.transform([[6]]))[0]) # 67
